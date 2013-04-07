be0123213a76b676779f37a0fc7b55504c620365
Neil Sarkar <neil@groupme.com>
2013-04-06 22:26:20 -0400

    Create tapestries

--- app.rb
+++ app.rb
@@ -8,22 +8,23 @@ require 'uri'
 require 'open-uri'
 
 require './lib/ripper'
+require './lib/uploader'
 
 get "/" do
   slideshows = Yajl::Parser.parse(open("http://www.funnyordie.com/json/media/slideshows").read)
 
   html = <<-HTML
   <body style="text-align:center">
-  <h1>I don't wanna wait</h1>
+  <h1>&#9835;I don't wanna wait&#9835;</h1>
   <img src="http://public0.ordienetworks.com/assets/vandermemes/vandermeme-25-8695f522b4c5a44b871a73567cf1e70a.gif" />
-  <h1>For these slides to be over</h1>
+  <h1>&#9835;For these slides to be over&#9835;</h1>
   <div style="border-top: 1px solid black; padding-top: 20px; margin-top: 20px">
   HTML
 
   slideshows.each do |slideshow|
     html += <<-HTML
       <div style="text-align: center; margin-bottom: 20px">
-        <a href="/#{URI.escape(slideshow["web_url"])}">
+        <a href="/create?title=#{URI.escape(slideshow["title"])}&amp;uri=#{URI.escape(slideshow["web_url"])}">
           <img src="#{slideshow["thumb_url"].gsub(/fullsize/,"rectangle_large")}" style="width: 150px; display: block; margin: 0 auto"/>
           #{slideshow["title"]}
         </a>
@@ -35,9 +36,17 @@ get "/" do
   HTML
 end
 
-get "/*" do
+get "/create" do
   # sinatra inexplicably stripping the first slash in http://
-  uri = params[:splat].first.gsub(/^http.*www/, "http://www")
-  ripper = Ripper.new(URI.unescape(uri))
-  ripper.html
+  title = URI.unescape(params[:title])
+  uri = URI.unescape(params[:uri]).gsub(/^http.*www/, "http://www")
+  ripper = Ripper.new(uri)
+
+  uploader = Uploader.new({
+    title: title,
+    html:  ripper.html,
+    css:   ripper.css
+  })
+  uploader.upload
+  "<a href='#{uploader.url}'>#{uploader.url}</a>"
 end
--- lib/uploader.rb
+++ lib/uploader.rb
@@ -1,10 +1,22 @@
 class Uploader
+  attr_accessor :url
+
   def initialize(options)
-    @html = options.html
-    @css  = options.css
+    @html = options[:html]
+    @css  = options[:css]
+    @title = options[:title]
   end
 
   def upload
-    # HTTPClient.new.post("")
+    response = HTTPClient.new.post("http://tapestri.es/api/story/", body: {
+      title: @title,
+      description: "",
+      html: @html,
+      css: @css,
+      client_key: "fdc3f8ef08ae2b07cbf677ac87947e",
+      client_secret: "73afa39ac1b9613e4da700e128d205"
+    })
+
+    @url = "http://tapestri.es/f/#{Yajl::Parser.parse(response.body)["id"]}"
   end
 end
